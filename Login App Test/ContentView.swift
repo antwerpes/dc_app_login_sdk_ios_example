d60e231a143616a4bbb9c626388dd9546ef8e595
Benjamin Werker <benjamin.werker@gmail.com>
2020-11-20 12:00:24 +0100

    Initial Commit

---
+++ Login App Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login App Test
+//
+//  Created by Benjamin Werker on 20.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
